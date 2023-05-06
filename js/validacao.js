bdbc9d76c38170019b438d28a305923181667d7d
Gustavo <gustavolimamendes2@gmail.com>
2023-05-06 14:58:10 -0300

    validacoes customizadas

--- js/validacao.js
+++ js/validacao.js
@@ -1,15 +1,21 @@
-const dataNascimento = document.querySelector('nascimento')
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo
 
-dataNascimento.addEventListener('blur', (evento) => {
-    validaDatanascimento(evento.target)
-})
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input)
+    }
+}
 
-function validaDatanascimento(input) {
+const validadores = {
+    dataNascimento:input => validaDataNascimento(input)
+}
+
+function validaDataNascimento(input) {
     const dataRecebida = new Date(input.value)
     let mensagem = ''
 
-    if (!maiorQue18(dataRecebida)){
-        mensagem = "Você deve ser maior que 18 anos para se cadastrar."
+    if(!maiorQue18(dataRecebida)) {
+        mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
     }
 
     input.setCustomValidity(mensagem)
@@ -17,8 +23,7 @@ function validaDatanascimento(input) {
 
 function maiorQue18(data) {
     const dataAtual = new Date()
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMounth(), data.getUTCDate())
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate())
 
     return dataMais18 <= dataAtual
-    
 }
